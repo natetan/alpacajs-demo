edc6e64fc214c977828e3fe6e138e3f2233e67dd
Yulong Tan <yulongtan67@gmail.com>
2017-07-18 17:17:44 -0700

    adds main form data and post renders for css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	
 	$('#form').alpaca({
 		'schema': {
-			'title':'Alaska Air (AS) DSO Data',
+			'title':'SkyWest (OO) DSO Data',
 			'description': 'DSO Inputs for airlines',
 			'type':'object',
 			'properties': {
@@ -14,8 +14,141 @@ $(document).ready(function() {
 					'type':'string',
 					'title': 'Booked Load Factor'
 				},
+				'atc': {
+					'type':'string',
+					'title': 'Air Traffic Control (ATC)'
+				},
+				'weather': {
+					'type':'string',
+					'title': 'Weather (WX)'
+				},
+				'diversions': {
+					'type':'string',
+					'title': 'Diversions'
+				},
+				'cancellations': {
+					'type':'string',
+					'title': 'Cancellations (XLS)'
+				},
+				'ots': {
+					'type':'string',
+					'title': 'Out of Service (OTS)'
+				},
+				'spares': {
+					'type':'string',
+					'title': 'Spares'
+				},
 			},
+		},
+		'options': {
+			'form': {
+				'buttons': {
+					'submit': {
+						'title': 'Test Button',
+						'click': test,
+					}
+				},
+			},
+			'fields': {
+				'comments': {
+					'placeholder': 'Please enter some comments here.',
+					'type': 'textarea',
+					'rows': 3
+				},
+				'blf': {
+					'type': 'textarea',
+					'rows': 3
+				},
+				'atc': {
+					'type': 'textarea',
+					'rows': 3
+				},
+				'weather': {
+					'type': 'textarea',
+					'rows': 3
+				},
+				'diversions': {
+					'type': 'textarea',
+					'rows': 3
+				},
+				'cancellations': {
+					'type': 'textarea',
+					'rows': 3
+				},
+				'ots': {
+					'type': 'textarea',
+					'rows': 3
+				},
+				'spares': {
+					'type': 'textarea',
+					'rows': 3
+				},
+			}
 		}
 	});
 
+	/*
+
+	Temporary fix to adding css to the Alpaca generated button
+
+	*/
+
+	postRenderButton('.alpaca-form-buttons-container', '.alpaca-form-button');
+
+	function postRenderButton(div, btn) {
+
+		$(div).css({
+			textAlign: 'left',
+		})
+
+		$(btn).removeClass('btn-default');
+
+		$(btn).css({
+			width: '200px',
+			height: '44px',
+			fontSize: '14px',
+			fontWeight: 'bold',
+			letterSpacing: '0.2px',
+			textAlign: 'center',
+			color: '#ffffff',
+			backgroundColor: '#48850b',
+			boxShadow: '0 0 3px 0 rgba(0, 0, 0, 0.3)',
+		});
+	}
+
+	/*
+	Temporary fix end
+	*/
+
+	// $('#verify-button').on('click', verify);
+
+	// function verify() {
+	// 	console.log('Button was clicked');
+	// 	var val = $('#form').getValue();
+	// 	alert(JSON.stringify(val, null, " "));
+	// }
+
+	function test() {
+		console.log('button was clicked');
+		var val = this.getValue();
+		if (this.isValid(true)) {
+			alert("Valid value: " + JSON.stringify(val, null, "  "));
+		} else {
+			alert("Invalid value: " + JSON.stringify(val, null, "  "));
+		}
+	}
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
